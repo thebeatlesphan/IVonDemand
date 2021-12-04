174b1d07310d0367e0ae36a3fb1584eca091fd6d
Home Dingo <aphanit@protonmail.com>
2021-12-04 00:48:52 -0500

    Reworked website in Bootstrap

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,19 @@
-function myFunction() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+//function myFunction() {
+//    var x = document.getElementById("links");
+//    if (x.style.display === "block") {
+//        x.style.display = "none";
+//    } else {
+//        x.style.display = "block";
+//    }
+//}
+//
+//function myLogin() {
+//    var x = document.getElementById("login-form");
+//    var y = document.getElementById("login");
+//    if (x.style.display === "block") {
+//        x.style.display = "none";
+//    } else {
+//        x.style.display = "block";
+//        y.style.display = "none";
+//    }
+//}
