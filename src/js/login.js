4b08872523eba62834fb45f2a4cb5d3f0fe06fd9
Home Dingo <aphanit@protonmail.com>
2021-12-06 15:28:51 -0500

    testing login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,19 +1,9 @@
-//function myFunction() {
-//    var x = document.getElementById("links");
-//    if (x.style.display === "block") {
-//        x.style.display = "none";
-//    } else {
-//        x.style.display = "block";
-//    }
-//}
-//
-//function myLogin() {
-//    var x = document.getElementById("login-form");
-//    var y = document.getElementById("login");
-//    if (x.style.display === "block") {
-//        x.style.display = "none";
-//    } else {
-//        x.style.display = "block";
-//        y.style.display = "none";
-//    }
-//}
+// import fetch from 'node-fetch';
+
+const ele = document.querySelector('form');
+ele.addEventListener('submit', event => {
+    const pw = document.getElementById("password");
+    console.log(pw.value);
+    event.preventDefault();
+})
+
